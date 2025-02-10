6cce2eea57cdccde11bfa2af161b9a6792c39f87
janisadhi <adhikarihjanis@gmail.com>
2025-02-10 21:35:44 +0545

    cdasdf

--- electro_map/dashboard/views.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-@login_required(login_url='/login/')  # Redirect to login page if not authenticated
-def index(request):
-    return render(request, 'dashboard.html')
